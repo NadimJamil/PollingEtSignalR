218d852a91f259778dac48434a10c79940b74908
Nadim Jamil <2261561@cegepmontpetit.ca>
2024-10-02 15:53:04 -0400

    FCT Continuation labo signal R

---
+++ PollingEtSignalR/labo.signalr.api/Hubs/SignalRHub.cs
@@ -0,0 +1,50 @@
+﻿using labo.signalr.api.Data;
+using labo.signalr.api.Models;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+
+namespace labo.signalr.api.Hubs
+{
+    public static class UserHandler
+    {
+        public static HashSet<String> ConnectedIds = new HashSet<string>();
+    }
+    public class SignalRHub : Hub
+    {
+        ApplicationDbContext _context;
+        public SignalRHub(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await base.OnConnectedAsync();
+            UserHandler.ConnectedIds.Add(Context.ConnectionId);
+            await Clients.All.SendAsync("UserCount", UserHandler.ConnectedIds.Count);
+            await Clients.Caller.SendAsync("TaskList", _context.UselessTasks.ToList());
+        }
+
+        public async Task AddTask(string task)
+        {
+            _context.UselessTasks.Add(new UselessTask() { Text = task });
+            _context.SaveChanges();
+            await Clients.All.SendAsync("TaskList", _context.UselessTasks.ToList());
+        }
+
+        public async Task TaskDone(int taskId)
+        {
+            var task = _context.UselessTasks.Single(t => t.Id == taskId);
+            task.Completed = true;
+            _context.SaveChanges();
+            await Clients.All.SendAsync("TaskList", _context.UselessTasks.ToList());
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            UserHandler.ConnectedIds.Remove(Context.ConnectionId);
+            await Clients.All.SendAsync("UserCount", UserHandler.ConnectedIds.Count);
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}
--- ngPollingEtSignalR/src/app/signalr/signalr.component.ts
+++ ngPollingEtSignalR/src/app/signalr/signalr.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import * as signalR from "@microsoft/signalr"
 import { UselessTask } from '../models/UselessTask';
 
@@ -21,18 +21,34 @@ export class SignalrComponent implements OnInit {
 
   connecttohub() {
     // TODO On doit commencer par créer la connexion vers le Hub
-
+    this.hubConnection = new signalR.HubConnectionBuilder().withUrl('http://localhost:5042/').build();
     // TODO On peut commencer à écouter pour les évènements qui vont déclencher des callbacks
-
+    this.hubConnection!.on('UneFonction', (data) =>{
+      this.usercount = data;
+    });
+    this.hubConnection.on('TaskList', (data) => {
+      console.log(data);
+      this.tasks = data;
+    });
     // TODO On doit ensuite se connecter
+    this.hubConnection
+    .start()
+    .then(() => {
+      console.log('La connexion est live!');
+
+    })
+    .catch(err => console.log('Error while starting connection: ' + err))
   }
 
   complete(id: number) {
     // TODO On invoke la méthode pour compléter une tâche sur le serveur
+    this.hubConnection!.invoke('CompleteTask', id);
   }
 
   addtask() {
     // TODO On invoke la méthode pour ajouter une tâche sur le serveur
+    this.hubConnection!.invoke('AddTask', this.taskname);
   }
 
 }
+
